7e76cdeda9cf9431caad2370a51a119cd9306fc3
nicodrose <ndesro@gmail.com>
2024-01-04 17:27:32 -0800

    Includes new functionality

--- routes/skills.js
+++ routes/skills.js
@@ -9,6 +9,8 @@ const skillsCtrl = require('../controllers/skills');
 /* This route is a GET request for 
 // GET /skills (our index action/functionality - list all skills)*/
 router.get('/', skillsCtrl.index);
+// GET /skills/new <--Define BEFORE show route
+router.get('/new', skillsCtrl.new);
 // GET /skills/:id (our show action/functionality, to display or show a single skill)
 router.get('/:id', skillsCtrl.show);
 
