7ceda9a39afcf47e8b0840894f0c95e42aaa3dba
nicodrose <ndesro@gmail.com>
2024-01-04 16:28:45 -0800

    Included partials

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //* This is a GET request to the root route; GET / (root / landing page) */
 // Change line 7 title from 'Express' to 'Express Dev Skills'
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Dev Skills' });
+  res.render('index', { title: 'Home Page' });
 });
 
 module.exports = router;
